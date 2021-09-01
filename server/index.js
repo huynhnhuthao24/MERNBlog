16f5c3a1a554f6b9daf57e96da9a57d489021a4d
Asland <huynhnhuth53@gmail.com>
2021-09-01 16:38:23 +0700

    push code MERNBlog

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import posts from './routers/posts.js';
+import mongoose from 'mongoose';
+
+
+
+const app = express();
+const PORT = process.env.port || 5000;
+const URI = 
+'mongodb+srv://admin:TZghBaunCfi9H8HL@cluster0.mttkx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+app.use(bodyParser.json({ limit: '30mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit:'30mb' }));
+app.use(cors());
+
+app.use('/posts', posts);
+
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() =>{
+        console.log('connect success to DB');
+        app.listen(PORT, () => {
+            console.log("server is runing" + `${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log('err', err);
+    });
+
